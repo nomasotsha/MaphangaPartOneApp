47d39849e86e68dade841e8d3d70ec373209531a
mmpebe <kgaogelo12@gmail.com>
2023-04-18 09:09:58 +0000

    Added Recipe Class and properties and comments

---
+++ MaphangaPartOneApp/Recipe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaphangaPartOneApp
+{
+    internal class Recipe
+    {
+        private Ingredient[] arIngredient;
+        private Step[] arStep;
+
+        private Ingredient[] ArIngredient
+        {
+            get { return arIngredient; }
+            set { arIngredient = value; }
+        }
+        //step property 
+        private Step[] ArStep
+        {
+            get { return arStep; }
+            set { arStep = value; }
+        }
+    }
+}
